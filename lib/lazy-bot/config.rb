b0792653deddf367dd74aef9d1f8d7c6111fda57
v1z <91447+v1z4@users.noreply.github.com>
2023-10-15 16:12:00 +0300

    Fix for bot env

--- lib/lazy-bot/config.rb
+++ lib/lazy-bot/config.rb
@@ -2,7 +2,7 @@
 
 module LazyBot
   class Config
-    attr_reader :conf, :bot_username, :bot_env, :telegram_token, :actions_path, :repo_class
+    attr_reader :conf, :bot_username, :telegram_token, :actions_path, :repo_class
 
     def initialize(file, repo_class:)
       full_conf = YAML.load_file(file, aliases: true).deep_symbolize_keys
@@ -10,7 +10,6 @@ module LazyBot
       @conf = full_conf[env.to_sym]
 
       @bot_username = conf[:bot_username] || raise('Username is not set')
-      @bot_env = conf[:bot_env] || 'development'
       @telegram_token = conf[:telegram_token] || raise('Token is not set')
       @actions_path = conf[:actions_path] || raise('Actions path is not set')
       @repo_class = repo_class
